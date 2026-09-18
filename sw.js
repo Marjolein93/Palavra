2d241457ba0091e225649d7283acdf73c8c20642
Marjolein <marjolein_minkman@hotmail.com>
2026-09-18 15:52:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'palavra-cache-v2';
+const CACHE_NAME = 'palavra-cache-v4';
 const STATIC_ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', (event) => {
